5cdbcee2df6d62e1759748c80ee24fe9f5843361
guedesbgeorge <guedesbgeorge@gmail.com>
2017-06-27 15:28:46 -0300

    Criando interface de pesquisa

--- searchUI/js/functions.js
+++ searchUI/js/functions.js
@@ -1,6 +1,10 @@
 function print() {
-  	var data = 'Nome: ' + $('#nome').val() + "    Preço: " + $("#preco").slider().val();
-  	window.java.print(data);
+	var nome = $('#nome').val();
+	var preco = $("#preco").slider().val();
+	var bateria = $("#bateria").slider().val();
+	var so = $('#so').selectpicker().val();
+	var conectividade = $('#conectividade').selectpicker().val();
+  	window.java.pesquisar(nome, preco, bateria, so, conectividade);
 }
 
 function changeButtonTitle(title) {
--- src/userinterface/SearchInterface.java
+++ src/userinterface/SearchInterface.java
@@ -1,6 +1,7 @@
 package userinterface;
 
 import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.JSArray;
 
 public class SearchInterface {
 	private Browser browser;
@@ -8,8 +9,21 @@ public class SearchInterface {
 	public SearchInterface(Browser browser) {
 		this.browser = browser;
 	}
-	public void print(String message) {
-		System.out.println(message);
-		browser.executeJavaScript("changeButtonTitle('" + message + " from eclipse')");
+	public void pesquisar(String nome, String preco, String bateria, String so, JSArray conectividade) {
+		System.out.println("Nome: " + nome);
+		System.out.println("Preço: R$ " + preco);
+		System.out.println("Bateria (mAh): " + bateria);
+		System.out.println("Sistema Operacional: " + so);
+		
+		String conect = "";
+		for (int i = 0; i < conectividade.length(); i++) {
+			conect += conectividade.get(i).getStringValue();
+			
+			if(i < conectividade.length() - 1)
+				conect += ", ";
+		}
+		System.out.println("Conectividade: " + conect);
+		
+		browser.executeJavaScript("changeButtonTitle('Worked from eclipse!')");
 	}
 }
