d737f914a0963d227a7596d32dca2b08c48d454e
imvm <imvm@cin.ufpe.br>
2017-06-28 10:05:56 -0300

    query processing basic setup

---
+++ src/queryprocessing/QueryEvaluation.java
@@ -0,0 +1,18 @@
+package queryprocessing;
+
+import invertedList.InvertedIndex;
+
+/**
+ * Created by ianmanor on 28/06/17.
+ */
+public class QueryEvaluation {
+    private InvertedIndex invertedIndex;
+
+    public void documentRetrieval() {
+
+    }
+
+    public void termRetrieval() {
+
+    }
+}
---
+++ src/queryprocessing/VectorSpaceModel.java
@@ -0,0 +1,16 @@
+package queryprocessing;
+
+/**
+ * Created by ianmanor on 28/06/17.
+ */
+public class VectorSpaceModel {
+
+    public double similarity() {
+
+    }
+
+    public double rankingTFIDF() {
+        double ranking = 0;
+        return ranking;
+    }
+}
